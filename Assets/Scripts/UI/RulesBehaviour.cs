e88eb4f8b659a9690abc53aa4f3ddb13c1e41b9b
Rivemale Loïck <lo.rivemale@gmail.com>
2020-04-06 17:50:16 +0200

    Added rules to the game + their behaviour.

---
+++ Assets/Scripts/UI/RulesBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulesBehaviour : MonoBehaviour
+{
+    public Transform Player;
+    public float MaxDistance;
+
+    void Update()
+    {
+        // So we hide the rules
+        if (Vector3.Distance(Player.position, transform.position) > MaxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
